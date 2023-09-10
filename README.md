7a097e61e89133cc916a122db1533480232cfc80
Rui Lopes <rgl@ruilopes.com>
2023-09-10 12:08:51 +0100

    init

---
+++ README.md
@@ -0,0 +1,55 @@
+# Usage (Ubuntu 22.04 host)
+
+[![Lint](https://github.com/rgl/terraform-libvirt-debian-example/actions/workflows/lint.yml/badge.svg)](https://github.com/rgl/terraform-libvirt-debian-example/actions/workflows/lint.yml)
+
+Create and install the [base Debian 12 vagrant box](https://github.com/rgl/debian-vagrant).
+
+Install Terraform:
+
+```bash
+wget https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_amd64.zip
+unzip terraform_1.5.7_linux_amd64.zip
+sudo install terraform /usr/local/bin
+rm terraform terraform_*_linux_amd64.zip
+```
+
+Create the infrastructure:
+
+```bash
+terraform init
+terraform plan -out=tfplan
+time terraform apply tfplan
+```
+
+**NB** if you have errors alike `Could not open '/var/lib/libvirt/images/terraform_example_root.img': Permission denied'` you need to reconfigure libvirt by setting `security_driver = "none"` in `/etc/libvirt/qemu.conf` and restart libvirt with `sudo systemctl restart libvirtd`.
+
+Show information about the libvirt/qemu guest:
+
+```bash
+virsh dumpxml terraform_example
+virsh qemu-agent-command terraform_example '{"execute":"guest-info"}' --pretty
+virsh qemu-agent-command terraform_example '{"execute":"guest-network-get-interfaces"}' --pretty
+./qemu-agent-guest-exec terraform_example id
+./qemu-agent-guest-exec terraform_example uname -a
+ssh-keygen -f ~/.ssh/known_hosts -R "$(terraform output --raw ip)"
+ssh "vagrant@$(terraform output --raw ip)"
+```
+
+Destroy the infrastructure:
+
+```bash
+time terraform destroy -auto-approve
+```
+
+# Virtual BMC
+
+You can externally control the VM using the following terraform providers:
+
+* [vbmc terraform provider](https://registry.terraform.io/providers/rgl/vbmc)
+  * exposes an [IPMI](https://en.wikipedia.org/wiki/Intelligent_Platform_Management_Interface) endpoint.
+  * you can use it with [ipmitool](https://github.com/ipmitool/ipmitool).
+  * for more information see the [rgl/terraform-provider-vbmc](https://github.com/rgl/terraform-provider-vbmc) repository.
+* [sushy-vbmc terraform provider](https://registry.terraform.io/providers/rgl/sushy-vbmc)
+  * exposes a [Redfish](https://en.wikipedia.org/wiki/Redfish_(specification)) endpoint.
+  * you can use it with [redfishtool](https://github.com/DMTF/Redfishtool).
+  * for more information see the [rgl/terraform-provider-sushy-vbmc](https://github.com/rgl/terraform-provider-sushy-vbmc) repository.
---
+++ main.tf
@@ -0,0 +1,166 @@
+# see https://github.com/hashicorp/terraform
+terraform {
+  required_version = "1.5.7"
+  required_providers {
+    # see https://registry.terraform.io/providers/hashicorp/random
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    # see https://registry.terraform.io/providers/hashicorp/template
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+    # see https://registry.terraform.io/providers/dmacvicar/libvirt
+    # see https://github.com/dmacvicar/terraform-provider-libvirt
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.7.1"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+variable "prefix" {
+  type    = string
+  default = "terraform_example"
+}
+
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.7.1/website/docs/r/network.markdown
+resource "libvirt_network" "example" {
+  name      = var.prefix
+  mode      = "nat"
+  domain    = "example.test"
+  addresses = ["10.17.3.0/24"]
+  dhcp {
+    enabled = false
+  }
+  dns {
+    enabled    = true
+    local_only = false
+  }
+}
+
+# create a cloud-init cloud-config.
+# NB this creates an iso image that will be used by the NoCloud cloud-init datasource.
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.7.1/website/docs/r/cloudinit.html.markdown
+# see journactl -u cloud-init
+# see /run/cloud-init/*.log
+# see https://cloudinit.readthedocs.io/en/latest/topics/examples.html#disk-setup
+# see https://cloudinit.readthedocs.io/en/latest/topics/datasources/nocloud.html#datasource-nocloud
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.7.1/libvirt/cloudinit_def.go#L133-L162
+resource "libvirt_cloudinit_disk" "example_cloudinit" {
+  name      = "${var.prefix}_example_cloudinit.iso"
+  user_data = <<EOF
+#cloud-config
+fqdn: example.test
+manage_etc_hosts: true
+users:
+  - name: vagrant
+    passwd: '$6$rounds=4096$NQ.EmIrGxn$rTvGsI3WIsix9TjWaDfKrt9tm3aa7SX7pzB.PSjbwtLbsplk1HsVzIrZbXwQNce6wmeJXhCq9YFJHDx9bXFHH.'
+    lock_passwd: false
+    ssh-authorized-keys:
+      - ${jsonencode(trimspace(file("~/.ssh/id_rsa.pub")))}
+disk_setup:
+  /dev/sdb:
+    table_type: mbr
+    layout:
+      - [100, 83]
+    overwrite: false
+fs_setup:
+  - label: data
+    device: /dev/sdb1
+    filesystem: ext4
+    overwrite: false
+mounts:
+  - [/dev/sdb1, /data, ext4, 'defaults,discard,nofail', '0', '2']
+runcmd:
+  - sed -i '/vagrant insecure public key/d' /home/vagrant/.ssh/authorized_keys
+EOF
+}
+
+# this uses the vagrant debian image imported from https://github.com/rgl/debian-vagrant.
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.7.1/website/docs/r/volume.html.markdown
+resource "libvirt_volume" "example_root" {
+  name             = "${var.prefix}_root.img"
+  base_volume_name = "debian-12-amd64_vagrant_box_image_0.0.0_box.img"
+  format           = "qcow2"
+  size             = 66 * 1024 * 1024 * 1024 # 66GiB. the root FS is automatically resized by cloud-init growpart (see https://cloudinit.readthedocs.io/en/latest/topics/examples.html#grow-partitions).
+}
+
+# a data disk.
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.7.1/website/docs/r/volume.html.markdown
+resource "libvirt_volume" "example_data" {
+  name   = "${var.prefix}_data.img"
+  format = "qcow2"
+  size   = 6 * 1024 * 1024 * 1024 # 6GiB.
+}
+
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.7.1/website/docs/r/domain.html.markdown
+resource "libvirt_domain" "example" {
+  name    = var.prefix
+  machine = "q35"
+  cpu {
+    mode = "host-passthrough"
+  }
+  vcpu       = 2
+  memory     = 1024
+  qemu_agent = true
+  cloudinit  = libvirt_cloudinit_disk.example_cloudinit.id
+  xml {
+    xslt = file("libvirt-domain.xsl")
+  }
+  video {
+    type = "qxl"
+  }
+  disk {
+    volume_id = libvirt_volume.example_root.id
+    scsi      = true
+  }
+  disk {
+    volume_id = libvirt_volume.example_data.id
+    scsi      = true
+  }
+  network_interface {
+    network_id     = libvirt_network.example.id
+    wait_for_lease = true
+    addresses      = ["10.17.3.2"]
+  }
+  provisioner "remote-exec" {
+    inline = [
+      <<-EOF
+      set -x
+      id
+      uname -a
+      cat /etc/os-release
+      echo "machine-id is $(cat /etc/machine-id)"
+      hostname --fqdn
+      cat /etc/hosts
+      sudo sfdisk -l
+      lsblk -x KNAME -o KNAME,SIZE,TRAN,SUBSYSTEMS,FSTYPE,UUID,LABEL,MODEL,SERIAL
+      mount | grep ^/dev
+      df -h
+      EOF
+    ]
+    connection {
+      type        = "ssh"
+      user        = "vagrant"
+      host        = self.network_interface[0].addresses[0] # see https://github.com/dmacvicar/terraform-provider-libvirt/issues/660
+      private_key = file("~/.ssh/id_rsa")
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      disk[0].wwn,
+      disk[1].wwn,
+    ]
+  }
+}
+
+output "ip" {
+  value = length(libvirt_domain.example.network_interface[0].addresses) > 0 ? libvirt_domain.example.network_interface[0].addresses[0] : ""
+}
