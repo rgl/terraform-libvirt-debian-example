504035386a1d2dfde0fb33f76d5a288864f2a8f9
Rui Lopes <rgl@ruilopes.com>
2026-04-04 15:10:58 +0100

    upgrade dependencies

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # see https://github.com/hashicorp/terraform
 terraform {
-  required_version = "1.14.3"
+  required_version = "1.14.8"
   required_providers {
     # see https://registry.terraform.io/providers/hashicorp/random
     # see https://github.com/hashicorp/terraform-provider-random
     random = {
       source  = "hashicorp/random"
-      version = "3.8.0"
+      version = "3.8.1"
     }
     # see https://registry.terraform.io/providers/dmacvicar/libvirt
     # see https://github.com/dmacvicar/terraform-provider-libvirt
